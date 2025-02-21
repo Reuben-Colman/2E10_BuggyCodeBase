732dafcfaa5b37c5ec20a2d1e6c0e3e42cafe7a9
Reuben Colman <reubencolman05@gmail.com>
2025-02-21 23:39:27 +0000

    Update Sent_Data_Processing.ino

--- Bronze Code/driving_code/Sent_Data_Processing.ino
+++ Bronze Code/driving_code/Sent_Data_Processing.ino
@@ -2,7 +2,4 @@ void processingDistance(double distance){
   WiFiClient client2 = server2.available();
   j = (char) distance;
   server2.write(j);
-  //int distanceTraveled = int(distance); // Convert distance char to an integer
-  Serial.println(distance);
-  //Serial.println(distanceTraveled);
 }
