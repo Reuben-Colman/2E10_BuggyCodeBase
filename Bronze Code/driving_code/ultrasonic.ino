5d255c76c321a0d48a4046b98aa426bacb5301b8
Reuben Colman <reubencolman05@gmail.com>
2025-02-21 13:33:53 +0000

    Create ultrasonic.ino

---
+++ Bronze Code/driving_code/ultrasonic.ino
@@ -0,0 +1,11 @@
+int Ultrasonic() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+
+  return distance;
+}
